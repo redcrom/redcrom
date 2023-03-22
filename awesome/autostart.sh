22285bc228ec15d0c97dc73f3571d9f25655e473
redcrom <80071397+redcrom@users.noreply.github.com>
2023-03-22 03:51:34 -0700

    Add files via upload
    added thumb stick good stuff

---
+++ awesome/autostart.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash 
+
+lxsession &
+/usr/bin/emacs --daemon &
+nitrogen --restore &
+volumeicon &
+#nm-applet &
+nm-tray &
+
+### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+# 1. Uncomment to restore last saved wallpaper
+#xargs xwallpaper --stretch < ~/.xwallpaper &
+# 2. Uncomment to set a random wallpaper on login
+# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# 3. Uncomment to set wallpaper with nitrogen
